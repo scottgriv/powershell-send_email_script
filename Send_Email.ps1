8476bcad8e3c4b9bc9b3d2239d230e0a44b7737e
Scott Griv <scottgrivner@gmail.com>
2023-01-06 15:44:27 -0500

    Create Send_Email.ps1

---
+++ Send_Email.ps1
@@ -0,0 +1,13 @@
+# Created by: Scott Grivner
+# Website: scottgrivner.dev
+
+$EmailFrom = "from_email@your_domain.com"
+$EmailTo = "to_email@your_domain.com"  
+$Subject = "Test Subject"
+$Body = "Test Body"
+$SMTPServer = "smtp.your_domain.com"
+$SMTPClient = New-Object Net.Mail.SmtpClient($SmtpServer, 25)   
+#$SMTPClient.EnableSsl = $true    
+#$SMTPClient.Credentials = New-Object 
+#System.Net.NetworkCredential("your_login_email@your_domain.com", "")    
+$SMTPClient.Send($EmailFrom, $EmailTo, $Subject, $Body)
